6d56c7050248451165254e2cd79095f0697e058f
dily <mdeeokoye@gmail.com>
2025-11-06 21:36:44 +0000

    remove the backslash in one of the cors url

--- src/UserService.Api/Program.cs
+++ src/UserService.Api/Program.cs
@@ -68,7 +68,7 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
 var allowedOrigins = new[]
 {
     "https://web-client-zeta-six.vercel.app", 
-    "https://clereview.vercel.app/",
+    "https://clereview.vercel.app",
     "http://localhost:5173", 
     "https://clereview-dev.vercel.app",
     "http://localhost:3000"
